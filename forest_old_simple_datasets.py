0b16ba634174e46ed5d5435295a35a225a00794c
Morgall <nielshuber@gmx.net>
2025-08-28 16:44:29 +0200

    adding running each cval per less random forest

--- forest_old_simple_datasets.py
+++ forest_old_simple_datasets.py
@@ -14,8 +14,6 @@ if __name__ == "__main__":
     cores_to_use = 80
     number_of_estimators = 50
 
-    result_dict = dict()
-
     to_do_dict = dict() # add datasets to be run into this dict and choose key as dataset name
 
 
@@ -65,58 +63,59 @@ if __name__ == "__main__":
     
     for dataset_name, data in to_do_dict.items(): #.items() gives key, values
 
-        result_dict[dataset_name] = dict()
-
-        dir_path = f'results/{dataset_name}'
+        dir_path = f'results/less_random_forest_{dataset_name}/n_est{number_of_estimators}_depth{depth_rolling_tree}'
 
         features = data.drop(columns=['y'])
         targets = data['y']
 
         skf = StratifiedKFold(n_splits=folds_cross_val, shuffle=True, random_state=42)
 
-        i=1 # index for fold number
+        for run in range(5): #account for randomness boostrapping and subfeature selection
 
-        for train_idx, test_idx in skf.split(features, targets): #gives row indices
+            i=1 # index for fold number
 
-            result_dict[dataset_name][i] = dict()
+            for train_idx, test_idx in skf.split(features, targets): #gives row indices
 
-            # Create the directory if it doesn't exist
-            os.makedirs(f'{dir_path}/forest_n{number_of_estimators}_tree_depth_{depth_rolling_tree}/fold{i}', exist_ok=True)
+                # Create the directory if it doesn't exist
+                os.makedirs(f'{dir_path}/fold{i}', exist_ok=True)
 
-            print(dataset_name)
-            print(i)
-            
-            features_train = features.iloc[train_idx]
-            features_test = features.iloc[test_idx]
-            targets_train = targets.iloc[train_idx]
-            targets_test = targets.iloc[test_idx]
+                with open(f'{dir_path}/fold{i}/fold{i}_time_{dataset_name}_run{run+1}.txt', 'w') as f:
+                    pass  # This just creates/truncates the file
 
-            stacked_train = pd.concat([targets_train, features_train], axis=1, ignore_index=False)
-            #stacked_test = pd.concat([targets_test, features_test],axis=1, ignore_index=False)
+                print(dataset_name)
+                print(i)
+                
+                features_train = features.iloc[train_idx]
+                features_test = features.iloc[test_idx]
+                targets_train = targets.iloc[train_idx]
+                targets_test = targets.iloc[test_idx]
 
-            #feature_columns = stacked_train.columns[1:] #assuming labels are in first column, ensured trough move_targets_to_front_and_rename()
-            feature_columns = list(range(features_train.shape[1]))
+                stacked_train = pd.concat([targets_train, features_train], axis=1, ignore_index=False)
+                #stacked_test = pd.concat([targets_test, features_test],axis=1, ignore_index=False)
 
-            tree_kwargs = dict(depth=8, criterion='gini', target_label='y', features=list(feature_columns))
+                #feature_columns = stacked_train.columns[1:] #assuming labels are in first column, ensured trough move_targets_to_front_and_rename()
+                feature_columns = list(range(features_train.shape[1]))
 
-            start_time_forest = time.time()
-            forest = CustomEnsembleClassifier(n_estimators=number_of_estimators, tree_kwargs=tree_kwargs, random_state=42, cores_to_use=cores_to_use)
-            
-            forest.parallel_fit(features_train, targets_train)
-            y_pred = forest.predict(features_test)
-            
-            res = pd.DataFrame()
-            res['y'] = targets_test
-            res['prediction'] = y_pred
+                tree_kwargs = dict(depth=8, criterion='gini', target_label='y', features=list(feature_columns))
 
-            end_time_forest= time.time()
+                start_time_forest = time.time()
+                forest = CustomEnsembleClassifier(n_estimators=number_of_estimators, tree_kwargs=tree_kwargs, random_state=42, cores_to_use=cores_to_use)
+                
+                forest.parallel_fit(features_train, targets_train)
+                y_pred = forest.predict(features_test)
+                
+                res = pd.DataFrame()
+                res['y'] = targets_test
+                res['prediction'] = y_pred
 
-            with open(f'{dir_path}/forest_n{number_of_estimators}_tree_depth_{depth_rolling_tree}/fold{i}/fold{i}_forest_time_{dataset_name}.txt', 'w') as f:
-                f.write(f"Forest execution time for {number_of_estimators} estimators with depth {depth_rolling_tree} : {end_time_forest - start_time_forest} seconds\n")
+                end_time_forest= time.time()
 
-            with open(f'{dir_path}/forest_n{number_of_estimators}_tree_depth_{depth_rolling_tree}/fold{i}/fold{i}_forest_result_{dataset_name}.csv', 'w') as f:
-                f.write(str(res.to_csv()))
-            i+=1
+                with open(f'{dir_path}/fold{i}/fold{i}_time_{dataset_name}_run{run+1}.txt', 'a') as f:
+                    f.write(f"Forest execution time for {number_of_estimators} estimators with depth {depth_rolling_tree} : {end_time_forest - start_time_forest} seconds\n")
+
+                with open(f'{dir_path}/fold{i}/{dataset_name}_result_test_run{run+1}.csv', 'w') as f:
+                    f.write(str(res.to_csv()))
+                i+=1
     
     end_time_run = time.time()
     with open(f'total_runtime_all_datasets_old_forest.txt', 'w') as f:
