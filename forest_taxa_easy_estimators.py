fbe84dd85ce87d57a30da91c236d09805580258b
Morgall <nielshuber@gmx.net>
2025-09-02 13:58:00 +0200

    taxa easy 800, 1000 estimator run

--- forest_taxa_easy_estimators.py
+++ forest_taxa_easy_estimators.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     #criterion_loss = "misclassification"
     folds_cross_val = 5
     cores_to_use = 80
-    n_estimator_list = [100, 200, 600]
+    n_estimator_list = [800, 1000]
     sub_features = 'sqrt'
     if sub_features == None:
         sub_name = 'None'
