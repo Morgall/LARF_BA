da676416dc9dcf90cbbc2755098ed4acc78ed23f
Morgall <nielshuber@gmx.net>
2025-10-03 11:23:30 +0200

    decreasing subset size

--- forest_taxa_all_bigger_subset.py
+++ forest_taxa_all_bigger_subset.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     cores_to_use = 80
     n_estimator = 500
     # try 0,05 first and reduce amount of cores to 50  accou8nting for additional preprocessing/constraint matrix ram use when increasing feature subset per split
-    sub_feature_list = [0.05]  #[0.03, 0.05, 0.075] should amount to ca. 120, 200, 300 features for each split oppposed to ca. 66 using sqrt
+    sub_feature_list = [0.025]  #[0.025 ,0.03, 0.05, 0.075] should amount to ca. 100, 120, 200, 300 features for each split oppposed to ca. 66 using sqrt
     
     data = pd.read_csv("datasets/microbiome_taxa_counts_all/microbiome_taxa_counts_all_bin.csv")
     dataset_name = 'microbiome_taxa_all'
